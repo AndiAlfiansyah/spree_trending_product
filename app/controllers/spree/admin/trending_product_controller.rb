861605da7b70cbba873d95c8679cf1267fbd0d3f
AndiAlfiansyah <m.andi@41studio.com>
2017-11-02 17:38:56 +0700

    change slide to trending

--- app/controllers/spree/admin/trending_product_controller.rb
+++ app/controllers/spree/admin/trending_product_controller.rb
@@ -9,6 +9,14 @@ module Spree
 
       private
 
+      def location_after_save
+        if @trending_product.created_at == @trending_product.updated_at
+          edit_admin_trending_product_url(@trending_product)
+        else
+          admin_slides_url
+        end
+      end
+
       def trending_product_params
         params.require(:trending_product).permit(:name, :body, :link_url, :published, :image, :position, :product_id)
       end
