35b09517ded1050d14b36db9b54ab0fe6941b58e
AndiAlfiansyah <m.andi@41studio.com>
2017-11-03 10:05:47 +0700

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do
-    resources :trending_product do
+    resources :trending_products do
       collection do
         post :update_positions
       end
