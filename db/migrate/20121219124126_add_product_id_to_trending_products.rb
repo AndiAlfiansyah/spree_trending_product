9f12960c979705cdbb6a5a46c1b41b57c3e62f3c
AndiAlfiansyah <m.andi@41studio.com>
2017-11-06 14:14:07 +0700

    tet

--- db/migrate/20121219124126_add_product_id_to_trending_products.rb
+++ db/migrate/20121219124126_add_product_id_to_trending_products.rb
@@ -1,5 +1,5 @@
 
-class AddProductIdToTrendingProduct < ActiveRecord::Migration[5.0]
+class AddProductIdToTrendingProducts < ActiveRecord::Migration[5.0]
   def change
     add_column :spree_trending_products, :product_id, :integer
   end
